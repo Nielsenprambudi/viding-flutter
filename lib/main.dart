ea4660abfa2cf4fda341dedf1b5038a6389ef1ba
Nielsenprambudi <nielsenprambudiasallie@gmail.com>
2021-11-24 02:19:41 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,54 +16,63 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
       ),
       home: Scaffold(
-        body: Center(
-          child: Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              children: [
-                ListTile(
-                  title: Image.asset("assets/viding-logo.png"),
-                  
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Masukkan Username"
-                  ),
-                  validator: (String? value) {
-                    if(value == null || value.isEmpty) {
-                      return 'Username tidak boleh kosong';
-                    }
-                    return null;
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Masukkan Username"
+        body: Container(
+          alignment: Alignment.center,
+          child: Center(
+              child: Card(
+                clipBehavior: Clip.antiAlias,
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Image.asset("assets/viding-logo.png", fit: BoxFit.cover),
+                      contentPadding: const EdgeInsets.only(top: 50),
+                      
                     ),
-                    validator: (String? value) {
-                      if(value == null || value.isEmpty) {
-                        return 'Username tidak boleh kosong';
-                      }
-                      return null;
-                    },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        debugPrint("on pressed");
-                      },
-                      child: const Text("Submit"),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Masukkan Username"
+                        ),
+                        validator: (String? value) {
+                          if(value == null || value.isEmpty) {
+                            return 'Username tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                      ), 
                     ),
-                )
-              ], 
-            ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Masukkan kata sandi",  
+                        ),
+                        validator: (String? value) {
+                          if(value == null || value.isEmpty) {
+                            return 'Kata sandi tidak boleh kosong';
+                          }
+                          return null;
+                        },
+                        obscureText: true,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 16.0),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            debugPrint("on pressed");
+                          },
+                          child: const Text("Submit"),
+                        ),
+                    )
+                  ], 
+                ),
 
-          ),
-        ),
+              ),
+            ),
+          ) 
+        
       )
     );
   }
